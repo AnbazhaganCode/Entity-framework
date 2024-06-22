a13b7492619ff8a636f31af14e778e5a75a2faf5
AnbazhaganCode <151020067+AnbazhaganCode@users.noreply.github.com>
2024-06-22 12:41:25 +0530

    Add files via upload
    Class 3

---
+++ LINQ 3 - sum of all values/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //LINQ class three
+
+            // Q)  List the sum of all values from given array 
+            int[] marks = { 11, 4, 6, 1, 9, 2, 5, 8 };
+
+            //Ans)
+            var result = marks.Sum();  //Linq
+
+            Console.WriteLine(result);
+            
+            Console.ReadKey();
+
+            //Output)
+            //46
+        }
+    }
+}
