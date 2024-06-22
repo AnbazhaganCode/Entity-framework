c18090796b1fc7e7e64c30db85ed17817ecda705
AnbazhaganCode <151020067+AnbazhaganCode@users.noreply.github.com>
2024-06-22 13:06:44 +0530

    Add files via upload
    Class 4

---
+++ LINQ 4 - Odd values and Less then 5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //LINQ class three
+
+            // Q)  List the odd values where lessthen 6 from given array 
+            int[] marks = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            //Ans)
+            var result = marks.Where(n=> n<6 && n%2 != 0);  //Linq
+
+            foreach (var i in result)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+
+            //Output)
+            //1
+            //3
+            //5
+        }
+    }
+}
