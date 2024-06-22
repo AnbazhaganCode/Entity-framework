dd0ca04312e8e02c3c088c1313b774a452b84ecc
AnbazhaganCode <151020067+AnbazhaganCode@users.noreply.github.com>
2024-06-22 13:18:39 +0530

    Add files via upload
    class 5

---
+++ Linq 5 - where Contains a/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //LINQ class 5
+
+            // Q)  List the values where Contains 'a' from given array 
+            string[] marks = { "Anbu","Balu","Somu" };
+
+            //Ans)
+            var result = marks.Where(n=> n.Contains("a"));  //Linq
+
+            foreach (var i in result)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+
+            //Output)
+            //Balu
+        }
+    }
+}
